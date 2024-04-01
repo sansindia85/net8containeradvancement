91fa72ebb28d129d138de5b205b1e9de0e236174
Sandeep Kandula <sandeepkandula@gmail.com>
2024-04-01 12:41:20 +0530

    Overall

---
+++ hello-aspnetcore/hello-aspnetcore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace hello_aspnetcore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public string Get()
+        {
+           
+            return $"Hello {RuntimeInformation.OSDescription} on {RuntimeInformation.OSArchitecture}";            
+        }
+    }
+}
---
+++ hello-dotnet/hello-dotnet/Program.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello, {RuntimeInformation.OSDescription} on {RuntimeInformation.OSArchitecture}!");
